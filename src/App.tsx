1c030a83d7d020624f92fd497a7d7134125c83d8
psh549633 <psh549633@gmail.com>
2022-11-04 01:38:03 +0900

    [ADD] recoil 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import GlobalStyle from "./styles/global";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
+import { RecoilRoot } from "recoil";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Router>
-        <LeftNavBar />
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
+        <RecoilRoot>
+          <LeftNavBar />
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </RecoilRoot>
       </Router>
     </>
   );
