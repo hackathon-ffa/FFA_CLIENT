f864983e13d6ddcaa40b363acc8b2475af54976e
kimgyuha <kimgyuha25@gmail.com>
2022-11-04 05:43:52 +0900

    [ADD] 파일 연결 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import { RecoilRoot } from "recoil";
+import VacationPage from "./pages/VacationPage";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import OtherSignUpChange from "./pages/OtherSignUpPage";
 
 const App = () => {
   return (
@@ -11,10 +15,14 @@ const App = () => {
       <GlobalStyle />
       <Router>
         <RecoilRoot>
-          <LeftNavBar />
+          <LeftNavBar/>
           <Routes>
             <Route path="/home" element={<HomePage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<SignUp/>} />
+            <Route path="/vacation" element={<VacationPage/>} />
+            <Route path="/othersignup" element = {<OtherSignUpChange/>}/>
           </Routes>
         </RecoilRoot>
       </Router>
