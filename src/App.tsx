2c8e8cf23543c1e1855f76b49543b655c35a4404
psh549633 <psh549633@gmail.com>
2022-11-04 00:34:32 +0900

    [ADD] leftNavBar에 대시보드 추가 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import LeftNavBar from "./components/LeftNavBar";
 import GlobalStyle from "./styles/global";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <LeftNavBar />
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </Router>
     </>
