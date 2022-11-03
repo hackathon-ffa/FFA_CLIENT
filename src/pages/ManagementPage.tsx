67a3a86645ee4fe2b4aa8a399aba249c85bd167d
psh549633 <psh549633@gmail.com>
2022-11-04 04:58:56 +0900

    [ADD] 유저 관리 페이지 퍼블리싱 완료

--- src/pages/ManagementPage.tsx
+++ src/pages/ManagementPage.tsx
@@ -1,12 +1,54 @@
 import styled from "styled-components";
 import AdminNavBar from "../components/AdminNavBar";
+import ManagementFilter from "../components/ManagementFilter";
+import UserListTable from "../components/UserListTable";
 
 const ManagementPage = () => {
+  const ROW_INFO = [
+    { text: "이름", width: "15%" },
+    { text: "부서", width: "20%" },
+    { text: "상태", width: "15%" },
+    { text: "출근시간", width: "25%" },
+    { text: "퇴근시간", width: "25%" },
+  ];
+
+  const data = [
+    {
+      name: "박성현",
+      department: "개발팀",
+      state: "출근",
+      start: "09:03:27",
+      end: null,
+    },
+    {
+      name: "박성현",
+      department: "개발팀",
+      state: "출근",
+      start: "09:03:27",
+      end: null,
+    },
+    {
+      name: "박성현",
+      department: "개발팀",
+      state: "출근",
+      start: "09:03:27",
+      end: null,
+    },
+    {
+      name: "박성현",
+      department: "개발팀",
+      state: "출근",
+      start: "09:03:27",
+      end: null,
+    },
+  ];
+
   return (
     <>
       <AdminNavBar />
       <Wrapper>
-        <p>dsa</p>
+        <ManagementFilter />
+        <UserListTable data={data} rowInfo={ROW_INFO} />
       </Wrapper>
     </>
   );
@@ -14,7 +56,7 @@ const ManagementPage = () => {
 
 const Wrapper = styled.div`
   margin-left: 250px;
-  padding: 30px;
+  padding: 30px 60px;
 `;
 
 export default ManagementPage;
